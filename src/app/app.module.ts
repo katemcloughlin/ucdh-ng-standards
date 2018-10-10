097b6ccc117e4d4a10090fb2f49b8b8617b83f42
kmcloughlin <kmcloughlin@ucdavis.edu>
2018-10-10 08:50:31 -0700

    installed Material and theme, ngBootstrap, and NG Multi-Select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatTableModule,
+  MatSortModule,
+  MatDialogModule,
+  MatTooltipModule,
+  MatTabsModule,
+  MatStepperModule
+} from '@angular/material';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +25,21 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatSortModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatStepperModule,
+    NgbModule,
+    NgMultiSelectDropDownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
